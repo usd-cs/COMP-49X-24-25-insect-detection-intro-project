db8144d7ac196d3aa3365de2034473ca6fffaa28
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2024-11-06 11:37:05 -0800

    created unittest file for training program

---
+++ testing/trainingprogramunittest.py
@@ -0,0 +1,14 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from trainingprogram import TrainingProgram
+
+class TestUserInput(unittest.TestCase):
+    def test_valid_file(self):
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
