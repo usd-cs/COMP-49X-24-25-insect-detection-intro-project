8dc92cbb4fe39e0092e3760e684448aea6496216
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2024-11-06 11:59:58 -0800

    added two tests for prompting user for training input

--- testing/trainingprogramunittest.py
+++ testing/trainingprogramunittest.py
@@ -1,13 +1,21 @@
 import unittest
 import sys
 import os
+from unittest.mock import patch
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from trainingprogram import TrainingProgram
 
 class TestUserInput(unittest.TestCase):
-    def test_valid_file(self):
-
+    @patch("builtins.input", return_value="trainingfiles")
+    def test_valid_file(self, mock_input):
+        tp = TrainingProgram()
+        self.assertEqual(tp.trainingDirectoryPrompt(), "Directory exists.")
+    
+    @patch("builtins.input", return_value="invalid")
+    def test_invalid_file(self, mock_input):
+        tp = TrainingProgram()
+        self.assertEqual(tp.trainingDirectoryPrompt(), "Directory does not exist.")
     
 
 if __name__ == "__main__":
