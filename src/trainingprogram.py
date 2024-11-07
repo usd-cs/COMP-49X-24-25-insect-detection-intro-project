a7ea474cadf831025c64c4e7a015594b985411e0
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2024-11-06 21:53:03 -0800

    added comments

--- src/trainingprogram.py
+++ src/trainingprogram.py
@@ -8,6 +8,7 @@ import numpy as np
 class TrainingProgram:
     
     def __init__(self):
+        # initialize class vars
         self.trainX = None
         self.trainY = None
         self.testX = None
@@ -16,9 +17,12 @@ class TrainingProgram:
         self.width = 0
 
     def createTrainingDirectoryPrompt(self):
+        # load mnist dataset from keras
         (self.trainX, self.trainY), (self.testX, self.testY) = mnist.load_data()
+        # take height and width of images from shape property
         self.height = self.trainX.shape[1]
         self.width = self.trainX.shape[2]
+        # print shape values of each set
         print(f'Training images shape: {self.trainX.shape}')
         print(f'Training labels shape: {self.trainY.shape}')
         print(f'Test images shape: {self.testX.shape}')
--- testing/trainingprogramunittest.py
+++ testing/trainingprogramunittest.py
@@ -9,6 +9,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 from trainingprogram import TrainingProgram
 
 class TestUserInput(unittest.TestCase):
+
+    # dataset prints expected output
     @patch("sys.stdout", new_callable=io.StringIO)
     def testLoadsValidDataset(self, stdOut):
         tp = TrainingProgram()
@@ -16,11 +18,13 @@ class TestUserInput(unittest.TestCase):
         outValue = stdOut.getvalue().strip()
         self.assertEqual(outValue, 'Training images shape: (60000, 28, 28)\nTraining labels shape: (60000,)\nTest images shape: (10000, 28, 28)\nTest labels shape: (10000,)')
         
+    # dataset saves correct height for images
     def testSavesCorrectHeight(self):
         tp = TrainingProgram()
         tp.createTrainingDirectoryPrompt()
         self.assertEqual(tp.height, 28)
     
+    # dataset saves correct width for images
     def testSavesCorrectWidth(self):
         tp = TrainingProgram()
         tp.createTrainingDirectoryPrompt()
