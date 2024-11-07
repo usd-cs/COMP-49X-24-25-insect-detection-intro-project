0826b82f6e9bafb0ddbf038e32792f1ed7e64bd2
danieldaugbjergusd <ddaugbjerg@sandiego.edu>
2024-11-06 19:41:49 -0800

    added tests for program to save height and width of dataset images

--- testing/trainingprogramunittest.py
+++ testing/trainingprogramunittest.py
@@ -15,7 +15,16 @@ class TestUserInput(unittest.TestCase):
         tp.createTrainingDirectoryPrompt()
         outValue = stdOut.getvalue().strip()
         self.assertEqual(outValue, 'Training images shape: (60000, 28, 28)\nTraining labels shape: (60000,)\nTest images shape: (10000, 28, 28)\nTest labels shape: (10000,)')
+        
+    def testSavesCorrectHeight(self):
+        tp = TrainingProgram()
+        tp.createTrainingDirectoryPrompt()
+        self.assertEqual(tp.height, 28)
     
+    def testSavesCorrectWidth(self):
+        tp = TrainingProgram()
+        tp.createTrainingDirectoryPrompt()
+        self.assertEqual(tp.width, 28)
 
 if __name__ == "__main__":
     unittest.main()
