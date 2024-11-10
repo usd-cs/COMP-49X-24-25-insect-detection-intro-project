d909b86f70be68482062096c2d43f5966d89fd14
Thomas McKeown <tmckeown@sandiego.edu>
2024-11-10 14:52:55 -0800

    Wrote functionality for reading, checking, transforming, and classifying a file. Requires integration, but passes tests for proper functionality

--- src/testingprogram.py
+++ src/testingprogram.py
@@ -1,12 +1,13 @@
-
+from torchvision import models, transforms
+import torch
 
 class TestingProgram:
     
-    def __init__(self):
-        testImage = None
+    def __init__(self, height):
+        self.testImage = None
+        self.height = height
 
-
-    def takeInput():
+    def takeInput(self):
         """
         Takes user's terminal input of a file path and opens the image if it exists for processing
 
@@ -16,37 +17,44 @@ class TestingProgram:
 
         try:
             f = open(filePath, 'r')
-            testImage = f
+            self.testImage = f
             return True
 
         except:
             print("File not found")
             return False
         
-    def transformImage(image):
+    def transformImage(self):
         """
-        Transforms input image to match other inputs based on guidelines established by training program
+        Transforms input image to match other inputs using transformation defined by training algorithm
 
         Returns: transformed image
         """
-        pass
 
-    def classifyImage():
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.Resize(self.height),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # mean and stdev for RGB channels
+        ])
+
+        transformedImage = transform(self.testImage)
+
+        return transformedImage
+
+    def classifyImage(self):
         """
         Runs image through testing program and returns results
 
         Returns: (Letter identified, Percent accuracy)
         """
-        pass
 
-    def classificationStub(image):
+        return self.classificationStub(self.testImage)
+
+    def classificationStub(self, image):
         """
         Temporary method placeholder that will be replaced by actual classification functionality.
         Serves only for testing purposes
         """
         pass
-
-
-if __name__ == "__main__":
-    tp = TestingProgram()
-    tp.takeInput
--- testing/testingprogramunittest.py
+++ testing/testingprogramunittest.py
@@ -4,31 +4,32 @@ import io
 import sys
 import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from testingprogram import TestingProgram
 
-class TestUserChoice:
+class TestUserChoice(unittest.TestCase):
     
     @patch('builtins.input', return_value = "README.md")
     def testValidUserInput(self, mock_input):
-        tp = TestingProgram()
-        (result, fileObject) = tp.takeInput()
-        self.assertEqual(result, 1)
+        tp = TestingProgram(10)                 #height of 10 is a placeholder, has no consequences
+        result = tp.takeInput()
+        self.assertEqual(result, True)
 
     @patch('builtins.input', return_value = "DoesnotExist")
     def testInvalidUserInput(self, mock_input):
-        tp = TestingProgram()
-        (result, fileObject) = tp.takeInput()
-        self.assertEqual(result, 0)
+        tp = TestingProgram(10)
+        result = tp.takeInput()
+        self.assertEqual(result, False)
 
     @patch.object(TestingProgram, 'classificationStub', return_value = ('A', .96))
-    def testCorrectLetterOutput(self):
-        tp = TestingProgram()
+    def testCorrectLetterOutput(self, mock_method):
+        tp = TestingProgram(10)
         (result, accuracy) = tp.classifyImage()
         self.assertEqual(result, 'A')
 
     @patch.object(TestingProgram, 'classificationStub', return_value = ('A', .96))
-    def testCorrectAccuracyOutput(self):
-        tp = TestingProgram()
+    def testCorrectAccuracyOutput(self, mock_method):
+        tp = TestingProgram(10)
         (result, accuracy) = tp.classifyImage()
         self.assertEqual(accuracy, .96)
 
